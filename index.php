4b9bde252c27ca66b20786a32908ce5d622a7328
Luis <soyluisbits@gmail.com>
2018-09-12 11:22:24 -0500

    Clase 2.5 - Arreglos

--- index.php
+++ index.php
@@ -1,6 +1,14 @@
 <?php
 $lastName = "Herrera";
 $name = "Luis $lastName";
+$jobs = [
+  [
+    'title' => 'PHP Developer',
+    'description' => 'This is a description'
+  ],
+  ['title' => 'Python Developer'],
+  ['title' => 'DevOps']
+];
 ?>
 
 <!doctype html>
@@ -51,8 +59,10 @@ $name = "Luis $lastName";
           <h3 class="border-bottom-gray" >Work Experience</h3>
           <ul>
             <li class="work-position">
-              <h5>PHP Developer</h5>
-              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi sapiente sed pariatur sint exercitationem eos expedita eveniet veniam ullam, quia neque facilis dicta voluptatibus. Eveniet doloremque ipsum itaque obcaecati nihil.</p>
+              <h5><?php echo $jobs[0]['title']; ?></h5>
+              <p>
+              <?php echo $jobs[0]['description']; ?>
+              </p>
               <strong>Achievements:</strong>
               <ul>
                 <li>Lorem ipsum dolor sit amet, 80% consectetuer adipiscing elit.</li>
@@ -61,7 +71,7 @@ $name = "Luis $lastName";
               </ul>
             </li>
             <li class="work-position">
-                <h5>PHP Developer</h5>
+                <h5><?php echo $jobs[1]['title']; ?></h5>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi sapiente sed pariatur sint exercitationem eos expedita eveniet veniam ullam, quia neque facilis dicta voluptatibus. Eveniet doloremque ipsum itaque obcaecati nihil.</p>
                 <strong>Achievements:</strong>
                 <ul>
@@ -71,7 +81,7 @@ $name = "Luis $lastName";
                 </ul>
               </li>
               <li class="work-position">
-                  <h5>PHP Developer</h5>
+                  <h5><?php echo $jobs[2]['title']; ?></h5>
                   <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi sapiente sed pariatur sint exercitationem eos expedita eveniet veniam ullam, quia neque facilis dicta voluptatibus. Eveniet doloremque ipsum itaque obcaecati nihil.</p>
                   <strong>Achievements:</strong>
                   <ul>
