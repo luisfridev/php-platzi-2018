28a2950024376e17e9bb5fab3f641821e08e7361
Luis <soyluisbits@gmail.com>
2018-09-12 10:59:55 -0500

    Tipos de datos y cadenas

--- index.php
+++ index.php
@@ -1,3 +1,8 @@
+<?php
+$lastName = "Herrera";
+$name = "Luis $lastName";
+?>
+
 <!doctype html>
 <html lang="en">
 
@@ -21,7 +26,7 @@
         <img id="profile-picture" src="https://ui-avatars.com/api/?name=John+Doe&size=255" alt="">
       </div>
       <div class="col">
-        <h1><?php echo 'Luis Angel';?></h1>
+        <h1><?php echo $name;?></h1>
         <h2>PHP Developer</h2>
         <ul>
           <li>Mail: hector@mail.com</li>
