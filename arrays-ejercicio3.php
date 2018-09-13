151f7f2de5f96d0e6d4d3aa936631e8e56f43149
Luis <soyluisbits@gmail.com>
2018-09-13 11:07:29 -0500

    Agregando 2da solucion a ejercicio 3

--- arrays-ejercicio3.php
+++ arrays-ejercicio3.php
@@ -9,7 +9,7 @@
 
 // Numeros más grandes
 $times = 3;
-echo 'Valores más grandes';
+echo 'Valores más grandes <br>';
 while ($times > 0) {
     echo max($valores). '<br>';
     $max_value_key = array_search(max($valores), $valores);
@@ -19,14 +19,29 @@ while ($times > 0) {
 
 // Numeros más bajos
 $times = 3;
-$valores = [23, 54, 32, 67, 34, 78, 98, 56, 21, 34, 57, 92, 12, 5, 61];
-echo 'Números más bajos';
+echo 'Números más bajos <br>';
 while ($times > 0) {
     echo min($valores). '<br>';
     $max_value_key = array_search(min($valores), $valores);
     unset($valores[$max_value_key]);
     $times--;
 }
+
+// Solucion 2
+function getValues($type, $times, $array_values)
+{
+    echo ucfirst($type).' numbers: <br>';
+    while ($times > 0) {
+    echo $type($array_values). '<br>';
+    $max_value_key = array_search($type($array_values), $array_values);
+    unset($array_values[$max_value_key]);
+    $times--;
+    }
+}
+
+$valores = [23, 54, 32, 67, 34, 78, 98, 56, 21, 34, 57, 92, 12, 5, 61];
+echo getValues('max',3,$valores);
+echo getValues('min',3,$valores);
 
 
 
