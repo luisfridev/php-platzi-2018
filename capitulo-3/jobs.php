d8b16dcda55347fc21334dd3bce8171dd8d9fa08
Luis <soyluisbits@gmail.com>
2018-09-16 18:51:48 -0500

    Cap 3.1 - Constructor y Metodos

--- capitulo-3/jobs.php
+++ capitulo-3/jobs.php
@@ -4,14 +4,23 @@ class Job
 {
     private $title;
     public $description;
-    public $visible;
+    public $visible = true;
     public $months;
+
+    public function __construct($title, $description)
+    {
+      $this->title = $this->setTitle($title);
+      $this->description = $description;
+    }
 
     /**
      * Asina valor a propiedad privada $title
      */
     public function setTitle($titleReceived)
     {
+      if ($titleReceived == '') {
+        $titleReceived = 'N/A';
+      }
       $this->title = $titleReceived;
     }
 
@@ -24,51 +33,19 @@ class Job
     }
 }
 
-$job1 = new Job();
-$job1->setTitle('PHP Developer');
-$job1->description = 'This is a description';
-$job1->visible = true;
+$job1 = new Job('PHP Developer', 'This is a description');
 $job1->months = 15;
 
-$job2 = new Job();
-$job2->setTitle('Python Developer');
-$job2->description = 'This is a description';
-$job2->visible = true;
+$job2 = new Job('Python Developer', 'This is a description');
 $job2->months = 13;
+
+$job3 = new Job('DevOps', 'This is a description');
+$job3->months = 13;
 
 $jobs = [
     $job1,
-    $job2
-  /* [
-    'title' => 'PHP Developer',
-    'description' => 'This is a description',
-    'visible' => true,
-    'months' => 15
-  ],
-  [
-    'title' => 'Python Developer',
-    'description' => 'This is a description',
-    'visible' => true,
-    'months' => 13
-  ],
-  [
-    'title' => 'DevOps',
-    'description' => 'This is a description',
-    'visible' => true,
-    'months' => 9
-  ],
-  [
-    'title' => 'NodeDev',
-    'description' => 'This is a description',
-    'visible' => false,
-    'months' => 10
-  ],
-  [
-    'title' => 'FrontDev',
-    'description' => 'This is a description',
-    'visible' => true,
-    'months' => 19
-  ] */
+    $job2,
+    $job3
 ];
 
 function getDuration($months)
@@ -92,7 +69,7 @@ function printJob($job)
   }
 
   echo '<li class="work-position">';
-  echo '<h5>'.$job->getTitle.'</h5>';
+  echo '<h5>'.$job->getTitle().'</h5>';
   echo '<p>'.$job->description.'</p>';
   echo '<p>'.getDuration($job->months).'</p>';
   echo '<strong>Achievements:</strong>';
