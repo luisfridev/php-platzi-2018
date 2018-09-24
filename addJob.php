fe0a4e7d9feced2bdce866f65fef75c70662b11b
Luis <holaluisbits@gmail.com>
2018-09-24 11:58:20 -0500

    Cap 5.5 Formularios

---
+++ addJob.php
@@ -0,0 +1,25 @@
+<?php
+
+var_dump($_POST);
+
+?>
+
+<html>
+    <head>
+        <title>Add Job</title>
+        <!-- Bootstrap CSS -->
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.2/css/bootstrap.min.css" integrity="sha384-Smlep5jCw/wG7hdkwQ/Z5nLIefveQRIY9nfy6xoR1uRYBtpZgI6339F5dgvm/e9B"
+        crossorigin="anonymous">
+        <link rel="stylesheet" href="style.css">
+    </head>
+    <body>
+        <h1>Add Job</h1>
+        <form action="addJob.php" method="post">
+            <label for="title">Title</label>
+            <input type="text" name="title" class="form-control">
+            <label for="description">Description</label>
+            <input type="text" name="description" class="form-control">
+            <button type="submit">Enviar</button>
+        </form>
+    </body>
+</html>
